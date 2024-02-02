2042813cb32715ad308c16fbc68bea2a04eb1bf9
Dylan Moss <dylan.moss2001@gmail.com>
2024-02-02 23:33:44 +0000

    Complete git authentication

--- docker/build.sh
+++ docker/build.sh
@@ -5,9 +5,9 @@ source ./config/container_config
 
 # Run container manager (either Docker or podman) with config args 
 $container_manager build -t $container_name \
-  --build-arg terminal_name=$terminal_name \
-  --build-arg name=$name \
-  --build-arg password=$password \
+  --build-arg terminal_name="$terminal_name" \
+  --build-arg name="$name" \
+  --build-arg password="$password" \
   --build-arg user_id=$(id -u) \
   --build-arg group_id=$(id -g) . \
-  --build-arg private_ssh_key_location=$private_ssh_key_location
+  --build-arg private_ssh_key="$private_ssh_key"
